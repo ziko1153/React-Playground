857ce6478d943f35e626f0acd31b59ced0f4ecff
Ziko1153 <tahmidziko@gmail.com>
2020-05-18 19:27:22 +0600

    Add new Color Fucntionalitis

--- react-timecounter-hooks/src/components/CounterHooks.js
+++ react-timecounter-hooks/src/components/CounterHooks.js
@@ -2,25 +2,28 @@ import React,{useState,useEffect} from 'react'
 
 function CounterHooks() {
 
-    const [counterObj,setCount] = useState({sec:50,min:59,hour:20,day:1,color:''});
-    const oddColor = ['red','violet','blue'];
-    const evenColor = ['pink','yellow','brown'];
+    const [counterObj,setCount] = useState({sec:0,min:0,hour:0,day:0,color:''});
+    const oddColor = ['OrangeRed','DarkMagenta ','DarkBlue'];
+    const evenColor = ['MediumSpringGreen','gold','Salmon'];
 
     const tick = () => {
         setCount(preCount => {
             let setColor;
-            if(preCount.sec%2==0) preCount.setColor = oddColor[Math.floor(Math.random()*oddColor.length)];
-            else preCount.setColor = evenColor[Math.floor(Math.random()*evenColor.length)];
+            /// This IS for Color Change .. Every Second Determine Even or Odd... 
+            if(preCount.sec%2==0) setColor = oddColor[Math.floor(Math.random()*oddColor.length)];
+            else setColor = evenColor[Math.floor(Math.random()*evenColor.length)];
 
-            if(preCount.sec==59 && preCount.min == 59 && preCount.hour <23) return {...preCount,sec:0,min:0,hour:preCount.hour+1}
-            else if(preCount.sec==59 && preCount.min <59) return {...preCount,sec:0,min:preCount.min+1}
-            else if(preCount.sec==59 && preCount.min == 59 && preCount.hour == 23) return {...preCount,sec:0,min:0,hour:0,day:preCount.day+1}
-            else return {...preCount,sec:preCount.sec+1}
+            /// Conditionally Return 
+            if(preCount.sec==59 && preCount.min == 59 && preCount.hour <23) return {...preCount,sec:0,min:0,hour:preCount.hour+1, color:setColor}
+            else if(preCount.sec==59 && preCount.min <59) return {...preCount,sec:0,min:preCount.min+1, color:setColor}
+            else if(preCount.sec==59 && preCount.min == 59 && preCount.hour == 23) return {...preCount,sec:0,min:0,hour:0,day:preCount.day+1, color:setColor}
+            else return {...preCount,sec:preCount.sec+1, color:setColor}
         })
 
         
     }
 
+    /// UseEffect Only One time USed in LifeCycle 
     useEffect(()=>{
         
         setInterval(()=> tick(),1000)
@@ -29,8 +32,8 @@ function CounterHooks() {
    
     return (
         <div className="counter">
-        <h2>{counterObj.color}</h2>
-    <h1>The Counting Time is : <span>{counterObj.day} Day</span>    <span>{counterObj.hour} hour</span>     <span>{counterObj.min} min</span> <span style={{color:counterObj.color}} >{counterObj.sec} </span>sec</h1>
+        <h2 style={{backgroundColor:counterObj.color,color:'white'}}>React Hooks Time Counter</h2>
+    <h1>The Counting Time is : <span>{counterObj.day} </span>   Day  <span>{counterObj.hour}</span>  hour    <span>{counterObj.min} </span>  min <span style={{color:counterObj.color}} >{counterObj.sec} </span>sec</h1>
         </div>
     )
 }
