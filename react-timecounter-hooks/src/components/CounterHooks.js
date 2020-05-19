337c9751179624fa9ba06811f72feff68cf07daa
Ziko1153 <tahmidziko@gmail.com>
2020-05-19 18:17:08 +0600

    add two new button for star time and stop time

--- react-timecounter-hooks/src/components/CounterHooks.js
+++ react-timecounter-hooks/src/components/CounterHooks.js
@@ -1,10 +1,13 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState,useEffect,useRef} from 'react'
 
 function CounterHooks() {
-
+   
     const [counterObj,setCount] = useState({sec:0,min:0,hour:0,day:0,color:''});
     const oddColor = ['OrangeRed','DarkMagenta ','DarkBlue'];
     const evenColor = ['MediumSpringGreen','gold','Salmon'];
+    const interval = useRef();
+    const [startButtonShow,setStartButtonShow] = useState(true)
+    const [stopButtonShow,setStopButtonShow] = useState(false)
 
     const tick = () => {
         setCount(preCount => {
@@ -23,18 +26,30 @@ function CounterHooks() {
         
     }
 
+    const timer = () =>{
+        clearInterval(interval.current)
+       return  interval.current =  setInterval(()=> tick(),1000);
+    }
     /// UseEffect Only One time USed in LifeCycle 
-    useEffect(()=>{
+    // useEffect(()=>{
         
-        setInterval(()=> tick(),1000)
-    },[])
+    //      interval.current = timer();
+    // },[])
 
    
     return (
+        <>
         <div className="counter">
         <h2 style={{backgroundColor:counterObj.color,color:'white'}}>React Hooks Time Counter</h2>
     <h1>The Counting Time is : <span>{counterObj.day} </span>   Day  <span>{counterObj.hour}</span>  hour    <span>{counterObj.min} </span>  min <span style={{color:counterObj.color}} >{counterObj.sec} </span>sec</h1>
+    {startButtonShow?<button className="start-btn" onClick={()=>{ setStartButtonShow(false);interval.current = timer()}}>Start Time</button>: <button className="stop-btn" onClick={()=>{ setStartButtonShow(true);clearInterval(interval.current)}}>Stop Time</button>}
+    
+
         </div>
+
+
+
+</>
     )
 }
 
